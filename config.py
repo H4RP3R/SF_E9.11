acdbeaee2f51d31924aaf74b5bddfd7969db9e90
H4RP3R <zombiehunter-vm@yandex.ru>
2020-09-29 12:58:34 +0300

    changes in db url config

--- config.py
+++ config.py
@@ -1,14 +1,8 @@
 import os
 
 
-POSTGRES_USER = os.environ.get('POSTGRES_USER')
-POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
-
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'aido2aGhielu0yuopieXoosoch6pheiV'
-    SQLALCHEMY_DATABASE_URI = \
-        f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_APP = 'event_planner.py'
